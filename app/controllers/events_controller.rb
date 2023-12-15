c8b209d0b1fa2f9e16b70e85cc0b60a3b4cf6b15
AranceLiu <fish70309@gmail.com>
2023-12-15 23:52:49 +0800

    WIP: Event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,22 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    
+  end
+
+  private
   
+  def event_params
+    params
+      .require(:event)
+      .permit(:subject, :start_date, :start_time, :end_date, :end_time, :all_day_event, :description, :location, :private)
+  end
 end
