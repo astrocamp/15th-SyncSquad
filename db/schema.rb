d41778e38b39de169f781cfb755b920eb8e1c253
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-16 00:31:07 +0800

    WIP: project CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_14_125515) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_15_140614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.integer "owner_id", null: false
+    t.datetime "delete_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["delete_at"], name: "index_projects_on_delete_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
