64c74b77dabfc6aef3cd9d334e57222eed55a0ce
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-16 05:33:44 +0800

    create model Room

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_22_043708) do
     t.index ["deleted_at"], name: "index_events_on_deleted_at"
   end
 
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.integer "max_participants"
+    t.boolean "is_private", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
