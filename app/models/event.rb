52585325306a1cb0db835de7e4ca72bda15ecdc9
AranceLiu <fish70309@gmail.com>
2023-12-16 17:26:09 +0800

    Event: model validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,36 @@ class Event < ApplicationRecord
   validates :subject, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :end_must_after_start
 
   # 關聯
   belongs_to :user
+
+
+
+  # 時間跟日期組合
+  def local_start_datetime(date, time)
+    Time.zone.parse("#{date} #{time}")
+  end
+
+  # 日期比對
+  def end_must_after_start
+    start_at = local_start_datetime(start_date, start_time)
+    end_at = local_start_datetime(end_date, end_time)
+    if end_at < start_at
+      errors.add(:end_date, 'must after the start time') 
+    end
+  end
+
+  # 行事曆格式
+  def full_calendar_event
+    {
+      id: id,
+      title: subject, 
+      start: local_start_datetime(start_date, start_time),
+      end: local_start_datetime(end_date, end_time),
+      allDay: all_day_event,
+      description: description,
+    }
+  end
 end
