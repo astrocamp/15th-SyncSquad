e17d2fb42180d20d42fa2cddebf892fbd4560a46
AranceLiu <fish70309@gmail.com>
2023-12-16 13:59:21 +0800

    Events: fix controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,9 @@
 class Event < ApplicationRecord
-  
+  # 驗證
   validates :subject, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
 
+  # 關聯
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Relationship
   has_many :events
 end
