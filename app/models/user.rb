9f6143d66952cfc1c4e71b86e74d88a82d3b7f66
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-17 13:00:45 +0800

    add model project_members

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # Relations
+  has_many :project_members
+  has_many :affilated_project, through: :project_members, source: :project
 end
