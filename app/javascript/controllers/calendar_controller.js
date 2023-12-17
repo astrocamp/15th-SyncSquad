42997755bc68f3cc4a95041a9a31bcfa885bbfe8
AranceLiu <fish70309@gmail.com>
2023-12-17 18:59:38 +0800

    Event: show on calendar

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -1,7 +1,24 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
 
 // Connects to data-controller="calendar"
 export default class extends Controller {
   connect() {
+    const calendarEle = this.element;
+    const calendar = new Calendar(calendarEle, {
+      plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+      initialView: "dayGridMonth", //以month形式
+      headerToolbar: {
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,listWeek",
+      },
+      events: "/events.json", //json
+    });
+    calendar.render();
   }
 }
