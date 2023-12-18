2afe4c0bac098702ac0c780318ab2f25497d6d1e
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-19 00:06:32 +0800

    fixed project relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Relationship
+
   has_many :events
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
