3d1965f09fabfb769e925e5da328f68c7e9d215b
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-19 01:11:51 +0800

    刪除測試相關和新增註解

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,28 +1,23 @@
 # frozen_string_literal: true
 
 class RoomsController < ApplicationController
+  # 要求登入帳號
   before_action :authenticate_user!
-
+  # 將new建立在index上
   def index
+    # 找到房間is_private: false 的欄位
     @rooms = Room.public_rooms
     @room = Room.new
+    # 找出除了現在登入的使用者
     @users = User.all_except(current_user)
     render 'index'
   end
 
   def create
     @room = Room.create(name: params['room']['name'])
-
-    # @room = Room.new(room_params)
-    # if @room.save
-    # 	redirect_to rooms_path, notice: 'Created new ChatRoom!'
-    # else
-    # 	render :new
-    # end
   end
 
   def show
-    find_room
     # Get user without current_user
     @single_room = Room.find(params[:id])
     @rooms = Room.public_rooms
@@ -32,28 +27,5 @@ class RoomsController < ApplicationController
     @message = Message.new
     @messages = @single_room.messages.order(created_at: :asc)
     render 'index'
-  end
-
-  def edit
-    find_room
-  end
-
-  def update
-    find_room
-    if @room.update
-      redirect_to rooms_path, notice: 'Updated ChatRoom Success!'
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def room_params
-    params.require(:room).permit(:name)
-  end
-
-  def find_room
-    @room = Room.find_by(id: params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,16 @@ class User < ApplicationRecord
 
   # validation for room,name is unique and couldn't talk to youself
   validates_uniqueness_of :name
+
   # Get users without appointment/special user
+  # scope 是一個 Rails 中用於定義範圍的方法。
+  # :all_except 是這個範圍的名稱。
+  # ->(user) { ... } 是一個 Lambda 函式，它接受一個參數 user。
+  # where.not(id: user) 它會從查詢結果中排除具有特定 id 的用戶。
   scope :all_except, ->(user) { where.not(id: user) }
+  # after_create_commit 自訂一個callback發生在create之後
+  # broadcast_append_to broadcast_append_to 是一個用於廣播的方法。
+  # 它將新創建的物件追加到前端與 'users' 標籤相關聯的部分
   after_create_commit { broadcast_append_to 'users' }
   has_many :messages
 end
