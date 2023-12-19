bbabe47a0a3ad2548d38bbd5727594836580fc46
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-19 15:43:39 +0800

    WIP: project turbo_stream

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,20 @@
 Rails.application.routes.draw do
-  resources :rooms do
-    resources :messages
-  end
+  root "main#home"
+  get 'main/home'
   
   devise_for :users 
   
   devise_scope :user do
     get 'users', to: 'devise/sessions#new'
   end
-    get 'user/:id', to:'users#show',as: 'user'
-    get 'main/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'user/:id', to:'users#show',as: 'user'
+  get 'main/home'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
   resources :events
-
-  
-  
-  # Defines the root path route ("/")
-  root "main#home"
   resources :projects
+
+  resources :rooms do
+    resources :messages
+  end
+  
 end
