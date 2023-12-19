29aa16f4679107f671ea40d238ab28a30f60a772
AranceLiu <fish70309@gmail.com>
2023-12-19 16:01:47 +0800

    Event: adjst from calendar

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -10,6 +10,7 @@ export default class extends Controller {
     flatpickr(this.endTimeTarget, {
       enableTime: true,
       noCalendar: true, // 不用日期
+
       minuteIncrement: 30,
       time_24hr: true,
     });
@@ -18,11 +19,13 @@ export default class extends Controller {
     flatpickr(this.startDateTarget, {
       enableTime: false,
       minDate: "today",
+
       // 選完開始日期就可以調整結束日期
       onClose: (selectedDates) => {
         flatpickr(this.endDateTarget, {
           enableTime: false,
           minDate: selectedDates[0] || "today",
+          defaultDate: selectedDates[0] || "today",
         });
       },
     });
@@ -30,6 +33,7 @@ export default class extends Controller {
     flatpickr(this.startTimeTarget, {
       enableTime: true,
       noCalendar: true, // 不用日期
+
       minuteIncrement: 30,
       time_24hr: true,
       // 選完開始時間就可以調整結束時間
@@ -55,6 +59,7 @@ export default class extends Controller {
     flatpickr(this.endDateTarget, {
       enableTime: false, // 不用時間
       minDate: "today",
+
       onClose: (selectedDates) => {
         flatpickr(this.endTimeTarget, {
           enableTime: true,
