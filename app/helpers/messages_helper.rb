22f9f580ff5dd5da4d123e2c8cd646acfb2ff20d
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-21 21:26:21 +0800

    markdown for message

---
+++ app/helpers/messages_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module MessagesHelper
+    def markdown(text)
+      renderer = Redcarpet::Render::HTML.new
+      markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+      markdown.render(text).html_safe
+      
+    end
+end
