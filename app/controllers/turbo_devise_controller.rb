56bbfc7d051c80ae85ee1b022a71b47d3b21e62a
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-21 12:59:38 +0800

    rubocop

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # frozen_string_literal: true
 
 class TurboDeviseController < ApplicationController
@@ -15,6 +16,25 @@ class TurboDeviseController < ApplicationController
     end
   end
 
+=======
+class TurboDeviseController < ApplicationController
+  class Responder < ActionController::Responder
+    # 重寫了 to_turbo_stream 方法
+    def to_turbo_stream
+      controller.render(options.merge(formats: :html))
+      # 當沒有辦法找到對應的 view
+    rescue ActionView::MissingTemplate => e
+      if get?
+        raise e
+      elsif has_errors? && default_action
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+      else
+        redirect_to navigation_location
+      end
+    end
+  end
+
+>>>>>>> 7b5e036 (rubocop)
   self.responder = Responder
   respond_to :html, :turbo_stream
 end
