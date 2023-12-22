eb4e8c913053b366fb2643f59119dacc35dc4759
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-22 12:19:25 +0800

    validation for messages and code snippet update

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Message < ApplicationRecord
+  validates :body, presence: true
   belongs_to :user
   belongs_to :room
   after_create_commit { broadcast_append_to room }
