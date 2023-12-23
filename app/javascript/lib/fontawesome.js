f983bba4cefb4aecc2d3d823b9a54b18a0e9592c
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-23 16:24:11 +0800

    modify project index layout

--- app/javascript/lib/fontawesome.js
+++ app/javascript/lib/fontawesome.js
@@ -1,5 +1,5 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faEllipsisVertical);
+library.add(faEllipsis);
 dom.watch();
