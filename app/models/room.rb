e4a79704a0018b52e0ef8d3be9100a07552f9b0f
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-23 13:59:30 +0800

    update model-room

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,6 +4,12 @@ class Room < ApplicationRecord
   # validation: room's name could not recurring and create public room
   validates :name, presence: true, uniqueness: true
   scope :public_rooms, -> { where(is_private: false) }
-  after_create_commit { broadcast_append_to 'rooms' }
+  # after_create_commit { broadcast_append_to 'rooms' }
+  after_create_commit{ broadcast_if_public }
   has_many :messages
+  has_many :participants, dependent: :destroy
+  
+  def broadcast_if_public
+    broadcast_append_to "rooms" unless self.is_private
+  end  
 end
