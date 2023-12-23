30d0852759400592064263c122a2b96908ad581a
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-23 15:15:24 +0800

    WIP some problem need to fix

--- app/models/room.rb
+++ app/models/room.rb
@@ -12,4 +12,12 @@ class Room < ApplicationRecord
   def broadcast_if_public
     broadcast_append_to "rooms" unless self.is_private
   end  
+
+  def self.create_private_rooms(users, room_name)
+    single_room = Room.create(name: room_name, is_private: true)
+    users.each do |user|
+      Participant.create(user_id: user.id, room_id: single_room.id)
+    end
+    single_room
+  end
 end
