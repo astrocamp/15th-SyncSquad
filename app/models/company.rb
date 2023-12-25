5f623c065171755476c22bb5d16609e29574d478
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-25 15:07:10 +0800

    encrypt password

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,14 @@ class Company < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, confirmation: true
+
+    before_create :encrypt_password
+  # 2FA 二階段驗證
+
+  private
+
+  def encrypt_password
+    salted_password = "*xx#{self.password}yy-"
+    self.password = Digest::SHA256.hexdigest(salted_password)
+  end
 end
