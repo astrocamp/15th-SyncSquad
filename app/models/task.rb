3da723e96ce4a79bb3b0cfb862758131b8c3fc39
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-25 10:54:13 +0800

    install ranked-model & add column to lists and tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,10 @@
 class Task < ApplicationRecord
   acts_as_paranoid
 
+  # Rank
+  include RankedModel
+  ranks :row_order, with_same: :list_id
+
   # Relations
   belongs_to :list
   has_many :users
