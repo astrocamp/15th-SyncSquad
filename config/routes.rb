4c4bc204d522c506da33617cd79dc72ccbe88632
Ruby Lin <ryoma510260@gmail.com>
2023-12-26 18:18:40 +0800

    WIP 406 problem

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root "main#home"
   get 'main/home'
   
-  devise_for :users
   
+  devise_for :users, controllers: {
+  registrations: 'users/registrations/registrations'
+}
+
   devise_scope :user do
     get 'users', to: 'devise/sessions#new'
   end
