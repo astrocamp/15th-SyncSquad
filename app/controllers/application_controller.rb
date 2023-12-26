b274c18713d06a0a374d4b1b1476f78c9e72bb6e
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-26 20:08:10 +0800

    change _flash.html.erb to _toast.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  add_flash_types :success, :info
 
   def not_found
     render file: Rails.public_path.join('404.html'),
