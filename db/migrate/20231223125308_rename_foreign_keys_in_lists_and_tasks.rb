cf8635334228eb7bc3a915548754480cab613e90
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-12-27 15:14:33 +0800

    fix migration of fixlistsAndTasks & change estimated_complete_at type to datetime

--- db/migrate/20231223125308_rename_foreign_keys_in_lists_and_tasks.rb
+++
@@ -1,6 +0,0 @@
-class RenameForeignKeysInListsAndTasks < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :lists, :projects_id, :project_id
-    rename_column :tasks, :lists_id, :list_id
-  end
-end
