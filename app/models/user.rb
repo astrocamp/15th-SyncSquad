45e973db32606bb9911213d302d2a14c4a06e876
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-31 01:56:17 +0800

    downsize for chat_avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
   end
 
   def chat_avatar
-    avatar.variant(resize_to_limit: [50, 50]).processed
+    avatar.variant(resize_to_limit: [35, 35]).processed
   end
   
   private
