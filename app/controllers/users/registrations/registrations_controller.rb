0585736acf7daa1fea82246ba50d07f521960cfb
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-30 17:33:57 +0800

    use sign_up and update params for devise

--- app/controllers/users/registrations/registrations_controller.rb
+++ app/controllers/users/registrations/registrations_controller.rb
@@ -4,7 +4,7 @@ module Users
   module Registrations
     class RegistrationsController < Devise::RegistrationsController
       before_action :configure_sign_up_params, only: [:create]
-      # before_action :configure_account_update_params, only: [:update]
+      before_action :configure_account_update_params, only: [:update]
 
       # GET /resource/sign_up
       # def new
@@ -15,15 +15,8 @@ module Users
       # def create
       #   super
       # end
+    
 
-      private
-
-      def configure_sign_up_params
-        devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation company_id])
-      end
-    end
-  end
-end
 # GET /resource/edit
 # def edit
 #   super
@@ -51,14 +44,14 @@ end
 # protected
 
 # If you have extra params to permit, append them to the sanitizer.
-# def configure_sign_up_params
-#   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-# end
+      def configure_sign_up_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation company_id avatar])
+      end
 
 # If you have extra params to permit, append them to the sanitizer.
-# def configure_account_update_params
-#   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-# end
+      def configure_account_update_params
+        devise_parameter_sanitizer.permit(:account_update, keys: %i[email password password_confirmation company_id avatar])
+      end
 
 # The path used after sign up.
 # def after_sign_up_path_for(resource)
@@ -69,3 +62,6 @@ end
 # def after_inactive_sign_up_path_for(resource)
 #   super(resource)
 # end
+    end
+  end
+end
