a9101950d50f2c83f9542d02c531e2aa4cdb90b1
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-30 17:36:32 +0800

    add default_avatarand avatar relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,32 @@ class User < ApplicationRecord
   has_many :affiliated_projects, through: :project_members, source: :project
   has_many :tasks
 
-  # Chatroom
+  # Chatroom messages
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
   has_many :messages
 
-  # Others
-  scope :all_except, ->(user) { where.not(id: user) }
-  after_create_commit { broadcast_append_to 'users' }
+  # Chatroom avatar
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: %i[create update]
+
+  # Project
+  has_many :project_members, dependent: :destroy
+  has_many :affiliated_projects, through: :project_members, source: :project
+  has_many :tasks
+  has_many :task_responsible_people, dependent: :destroy
+  has_many :in_charge_of_tasks, through: :task_responsible_people, source: :task
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_profile.png')),
+      filename: 'default_profile.png',
+      content_type: 'image/png'
+    )
+  end
 end
