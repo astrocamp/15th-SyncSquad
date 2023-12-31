986a1a608472890872b45e1786ccc8f82732c5e3
Tim Chaio <timmy1993jaw@gmail.com>
2023-12-31 15:34:16 +0800

    chat room user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   # Chatroom messages
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
+
   has_many :messages
 
   # Chatroom avatar
