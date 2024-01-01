8d775590ad7023215cc169596a9421872de27717
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-01 10:35:18 +0800

    impovement defult avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,15 +21,13 @@ class User < ApplicationRecord
   # Chatroom avatar
   has_one_attached :avatar
 
-  after_commit :add_default_avatar, on: %i[create update]
-
   # Project
   has_many :project_members, dependent: :destroy
   has_many :affiliated_projects, through: :project_members, source: :project
   has_many :tasks
   has_many :task_responsible_people, dependent: :destroy
   has_many :in_charge_of_tasks, through: :task_responsible_people, source: :task
-  
+
   def avatar_thumbnail
     resize_avatar(150, 150)
   end
@@ -50,13 +48,4 @@ class User < ApplicationRecord
     end
   end
 
-  def add_default_avatar
-    return if avatar.attached?
-
-    avatar.attach(
-      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_profile.png')),
-      filename: 'default_profile.png',
-      content_type: 'image/png'
-    )
-  end
 end
