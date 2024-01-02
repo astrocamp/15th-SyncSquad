1c4bab1fcc922366e8ff26121287d0ad2292ed98
Ruby Lin <ryoma510260@gmail.com>
2024-01-03 06:15:11 +0800

    I18n setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,17 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   add_flash_types :success, :info, :mail
   helper_method :current_company
+  around_action :switch_locale
 
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  def switch_locale(&action)
+    lang = params[:lang] || I18n.default_locale
+    I18n.with_locale(lang, &action)
+  end
+  
   def current_company
     Company.find_by(id: session[:__company_ticket__])
   end
