85061d9f2d66ad204d43cdcddcec93ebf14eea91
Ruby Lin <ryoma510260@gmail.com>
2024-01-02 13:09:26 +0800

    delete repeated migration

--- db/migrate/20231224023000_rename_foreign_key_in_task_responsible_person.rb
+++
@@ -1,6 +0,0 @@
-class RenameForeignKeyInTaskResponsiblePerson < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :task_responsible_people, :tasks_id, :task_id
-    rename_column :task_responsible_people, :users_id, :user_id
-  end
-end
