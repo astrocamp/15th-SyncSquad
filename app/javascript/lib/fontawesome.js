fe3faffc3e66bdddbccb6afd956812886d95a0dd
AranceLiu <fish70309@gmail.com>
2024-01-02 11:51:01 +0800

    fix: font awesome and typesetting

--- app/javascript/lib/fontawesome.js
+++ app/javascript/lib/fontawesome.js
@@ -1,5 +1,6 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faEllipsis, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faEllipsis);
+library.add(faPlus);
 dom.watch();
