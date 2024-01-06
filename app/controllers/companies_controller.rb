8fa8e5f395a78634fbe263c7340caa509a43e35f
Ruby Lin <ryoma510260@gmail.com>
2024-01-06 16:57:41 +0800

    feat: able to edit company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -17,6 +17,20 @@ class CompaniesController < ApplicationController
 
   def sign_in; end
 
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+
+    if @company.update(company_params)
+      redirect_to company_path, notice: '更新成功'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def company_params
