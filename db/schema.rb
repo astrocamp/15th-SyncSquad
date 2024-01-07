70700e24cb28f3f34a918c9256fbc0e0d31f71b9
Ruby Lin <ryoma510260@gmail.com>
2024-01-07 15:59:22 +0800

    test db reset

--- db/schema.rb
+++ db/schema.rb
@@ -202,7 +202,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_15_074426) do
     t.datetime "birthday", precision: nil
     t.string "phone"
     t.datetime "last_login_at", precision: nil
-    t.string "role"
+    t.integer "role"
     t.string "lang"
     t.string "time_zone"
     t.datetime "delete_at", precision: nil
