dc627437afc0dba806e046214d18989d5f35c966
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-08 05:08:08 +0800

    fix mseeage bugs

--- app/models/room.rb
+++ app/models/room.rb
@@ -23,7 +23,7 @@ class Room < ApplicationRecord
   end
 
   def self.create_private_room(users, room_name)
-    single_room = Room.create(name: room_name, is_private: true, room_type: 1)
+    single_room = Room.create(name: room_name, is_private: true, room_type: 'single_room')
     users.each do |user|
       Participant.create(user_id: user.id, room_id: single_room.id)
     end
