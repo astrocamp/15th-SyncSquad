777903f585d2ba480b8eaa3b85e74db35043ce07
Ruby Lin <ryoma510260@gmail.com>
2024-01-08 12:30:35 +0800

    Use: rubocop

--- db/migrate/20231227060512_fix_lists_and_tasks.rb
+++ db/migrate/20231227060512_fix_lists_and_tasks.rb
@@ -1,9 +1,5 @@
 class FixListsAndTasks < ActiveRecord::Migration[7.1]
   def change
-    #rename_column :lists, :projects_id, :project_id
-    #rename_column :tasks, :lists_id, :list_id
-    #rename_column :task_responsible_people, :tasks_id, :task_id
-    #rename_column :task_responsible_people, :users_id, :user_id
     add_column :tasks, :row_order, :integer
     add_column :lists, :row_order, :integer
     add_column :lists, :deleted_at, :datetime
