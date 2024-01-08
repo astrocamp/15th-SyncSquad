79409c76b7c4d06dbcf34d3715c0ac061a68aaeb
AranceLiu <fish70309@gmail.com>
2024-01-08 22:52:45 +0800

    chatroom: using enter to submit message

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -4,4 +4,13 @@ export default class extends Controller {
   reset() {
     this.element.reset();
   }
+  onEnter(event) {
+    if (event.code === "Enter" && !event.shiftKey) {
+      if (!event.isComposing) {
+        this.element.submit();
+        event.preventDefault();
+        this.element.reset();
+      }
+    }
+  }
 }
