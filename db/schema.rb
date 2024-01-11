a25075f73fba504865bf9db39d70d91a486e04c6
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-11 13:46:07 +0800

    add column for Omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_30_081541) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_11_054242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -165,6 +165,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_081541) do
     t.string "nick_name"
     t.datetime "deleted_at"
     t.integer "company_id"
+    t.string "provider"
+    t.string "uid"
+    t.string "full_name"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
