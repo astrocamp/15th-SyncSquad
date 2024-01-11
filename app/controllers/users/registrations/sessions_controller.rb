3ef4162572b38b881b884b9958b8fdedd902a58b
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-11 14:32:14 +0800

    add session signin/signout and omniauth callbacks

--- app/controllers/users/registrations/sessions_controller.rb
+++ app/controllers/users/registrations/sessions_controller.rb
@@ -27,6 +27,14 @@ module Users
         respond_to_on_destroy
       end
 
+      def after_sign_out_path_for(_resource_or_scope)
+        new_user_session_path
+      end
+
+      def after_sign_in_path_for(resource_or_scope)
+        stored_location_for(resource_or_scope) || root_path
+      end
+
       # protected
 
       # If you have extra params to permit, append them to the sanitizer.
