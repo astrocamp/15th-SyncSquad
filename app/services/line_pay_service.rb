0b8966190a8587bec9e105afcddf0ab6fc55e718
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-15 05:27:31 +0800

    run rubocop

--- app/services/line_pay_service.rb
+++ app/services/line_pay_service.rb
@@ -1,27 +1,30 @@
+# frozen_string_literal: true
+
 require 'httparty'
 class LinePayService
   def initialize
-    @merchant_id = "2002813423"
-    @merchant_key = "68e4d49082a0dc9d88a6819c7037b335"
-    @uri = "https://sandbox-api-pay.line.me" # 沙箱測試環境
+    @merchant_id = Rails.application.credentials.line_pay[:merchant_id].to_s
+    @merchant_key = Rails.application.credentials.line_pay[:merchant_key]
+
+    @uri = 'https://sandbox-api-pay.line.me' # 沙箱測試環境
   end
-  
+
   def request_payment
     headers = {
-      "Content-Type" => "application/json",
-      "X-LINE-ChannelId" => @merchant_id,
-      "X-LINE-ChannelSecret" => @merchant_key
+      'Content-Type' => 'application/json',
+      'X-LINE-ChannelId' => @merchant_id,
+      'X-LINE-ChannelSecret' => @merchant_key
     }
-  
+
     body = {
-      productName: "升級為VIP",
+      productName: '升級為VIP',
       amount: 699,
-      currency: "TWD",
-      confirmUrl: "http://localhost:5000/path_to_confirm_endpoint",
+      currency: 'TWD',
+      confirmUrl: 'http://localhost:5000/path_to_confirm_endpoint',
       orderId: SecureRandom.uuid # 生成唯一的訂單ID
-		}
-  
-    response = HTTParty.post("#{@uri}/v2/payments/request", headers: headers, body: body.to_json)
+    }
+
+    response = HTTParty.post("#{@uri}/v2/payments/request", headers:, body: body.to_json)
 
     if response.code == 200
       response
