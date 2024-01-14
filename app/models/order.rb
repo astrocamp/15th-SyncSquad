c145a0e7fdfda535d31a35238b63dabf2db72fbe
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-15 04:44:22 +0800

    add orders_controller and line_pay_service

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,23 @@
 class Order < ApplicationRecord
+
+  belongs_to :user
+  validates :serial_num, uniqueness: true
+
+  before_create :generate_serial
+
+  private
+
+  def generate_serial
+    self.serial_num = serial_generator
+  end
+
+  def serial_generator(digits = 6)
+    now = Time.current
+    year = now.year
+    month = format('%02d', now.month)
+    day = format('%02d', now.day)
+    code = SecureRandom.alphanumeric.upcase[0..digits - 1]
+
+    "Sync#{year}#{month}#{day}#{code}"
+  end
 end
