5b6e490b6045d8e3c2aa63363c0f3b7507d4a4c5
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-15 04:44:22 +0800

    add orders_controller and line_pay_service

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,32 @@
 class OrdersController < ApplicationController
+
+	
+	def index
+		@order = Order.new
+	end
+	
+	def create
+    line_pay_service = LinePayService.new
+    response = line_pay_service.request_payment
+
+    Rails.logger.info "LINE Pay response: #{response.parsed_response}"
+
+    if response && response.parsed_response["returnCode"] == "0000"
+        payment_url = response.parsed_response["info"]["paymentUrl"]["web"]
+				Rails.logger.info "Redirecting to LINE Pay at #{payment_url}"
+
+				respond_to do |format|
+            format.turbo_stream
+            format.html { redirect_to payment_url, allow_other_host: true } # 導向 LINE Pay 支付頁面
+        end
+    else
+        # 處理錯誤情況，重新渲染 index 頁面
+        @order = Order.new # 確保 index 頁面有 @order 變量
+        respond_to do |format|
+            format.turbo_stream # 需要一個對應的 .turbo_stream.erb 模板
+            format.html { render :index } # 改為重新渲染 index 頁面
+        end
+    end
 end
+
+	end
