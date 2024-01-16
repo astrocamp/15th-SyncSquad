200a25b47401014f56956db13f6889797b1a25b3
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2024-01-16 22:33:22 +0800

    add project policy

---
+++ app/policies/project_policy.rb
@@ -0,0 +1,19 @@
+class ProjectPolicy < ApplicationPolicy
+  
+  def show?
+    user.company == User.find(record.owner_id).company
+  end
+
+  def edit?
+    show? && user.id == record.owner_id
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy
+    edit?
+  end
+
+end
