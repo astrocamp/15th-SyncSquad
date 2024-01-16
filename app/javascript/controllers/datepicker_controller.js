39977a45588224cff0e32bedee9629bdce7d2304
AranceLiu <fish70309@gmail.com>
2024-01-16 21:41:53 +0800

    Tasks: flatpickr for Kanban and calendar

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,82 +4,99 @@ import dayjs from "dayjs";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
-  static targets = ["startDate", "startTime", "endDate", "endTime"];
+  static targets = ["startedAt", "endedAt", "allDay"];
 
   connect() {
-    // 結束時間的日曆雛形
-    flatpickr(this.endTimeTarget, {
-      enableTime: true,
-      noCalendar: true, // 不用日期
-      minTime: this.getAdjustedTime(),
-      minuteIncrement: 30,
-      time_24hr: true,
-    });
-
-    // 開始日期調整結束日期
-    flatpickr(this.startDateTarget, {
-      enableTime: false,
-      minDate: "today",
-      // 選完開始日期就可以調整結束日期
-      onChange: (selectedDates) => {
-        flatpickr(this.endDateTarget, {
-          enableTime: false,
-          minDate: selectedDates[0] || "today",
-          defaultDate: selectedDates[0] || "today",
-        });
-        // 選完開始日期就可以調整結束時間
-        flatpickr(this.endTimeTarget, {
-          enableTime: true,
-          noCalendar: true, // 不用日期
-          defaultDate: this.getAdjustedTime(),
-          minTime: this.getAdjustedTime(),
-          minuteIncrement: 30,
-          time_24hr: true,
-        });
-      },
-    });
-
-    // 開始時間調整結束時間
-    flatpickr(this.startTimeTarget, {
-      enableTime: true,
-      noCalendar: true, // 不用日期
-      minuteIncrement: 30,
-      time_24hr: true,
-      // 選完開始時間就可以調整結束時間
-      onChange: (selectedDates) => {
-        flatpickr(this.endTimeTarget, {
-          enableTime: true,
-          noCalendar: true, // 不用日期
-          defaultDate: this.getAdjustedTime(),
-          minTime: this.getAdjustedTime(),
-          minuteIncrement: 30,
-          time_24hr: true,
-        });
-      },
-    });
-
-    // 結束日期 配合 同一天
-    flatpickr(this.endDateTarget, {
-      enableTime: false, // 不用時間
-      minDate: "today",
-      onChange: (selectedDates) => {
-        flatpickr(this.endTimeTarget, {
-          enableTime: true,
-          noCalendar: true, // 不用日期
-          defaultDate: this.getAdjustedTime(),
-          minTime: this.getAdjustedTime(),
-          minuteIncrement: 30,
-          time_24hr: true,
-        });
-      },
-    });
+    if (this.allDayTarget.checked) {
+      flatpickr(this.endedAtTarget, {
+        enableTime: false,
+        defaultDate: this.endedAtTarget.value,
+        minDate: this.startedAtTarget.value,
+      });
+      flatpickr(this.startedAtTarget, {
+        enableTime: false,
+        defaultDate: this.startedAtTarget.value,
+        onChange: (selectedDates) => {
+          flatpickr(this.endedAtTarget, {
+            enableTime: false,
+            defaultDate: selectedDates[0],
+            minDate: selectedDates[0],
+          });
+        },
+      });
+    } else {
+      flatpickr(this.endedAtTarget, {
+        enableTime: true,
+        minuteIncrement: 30,
+        time_24hr: true,
+        defaultDate: this.endedAtTarget.value,
+        minDate: this.startedAtTarget.value,
+      });
+      flatpickr(this.startedAtTarget, {
+        enableTime: true,
+        minuteIncrement: 30,
+        time_24hr: true,
+        defaultDate: this.startedAtTarget.value,
+        onChange: (selectedDates) => {
+          flatpickr(this.endedAtTarget, {
+            enableTime: true,
+            minuteIncrement: 30,
+            time_24hr: true,
+            defaultDate: selectedDates[0],
+            minDate: selectedDates[0],
+          });
+        },
+      });
+    }
   }
 
-  getAdjustedTime() {
-    return this.startDateTarget.value === this.endDateTarget.value
-      ? dayjs(`${this.startDateTarget.value} ${this.startTimeTarget.value}`)
-          .add(5, "minute")
-          .format("HH:mm")
-      : "00:00";
+  clickedAllDay(event) {
+    if (event.target.checked) {
+      flatpickr(this.endedAtTarget, {
+        enableTime: false,
+        defaultHour: "00",
+        defaultMinute: "00",
+        defaultDate: this.endedAtTarget.value,
+        minDate: this.startedAtTarget.value,
+      });
+      flatpickr(this.startedAtTarget, {
+        enableTime: false,
+        defaultHour: "00",
+        defaultMinute: "00",
+        defaultDate: this.startedAtTarget.value,
+        onChange: (selectedDates) => {
+          flatpickr(this.endedAtTarget, {
+            enableTime: false,
+            defaultHour: "00",
+            defaultMinute: "00",
+            defaultDate: selectedDates[0],
+            minDate: selectedDates[0],
+          });
+        },
+      });
+    } else {
+      flatpickr(this.endedAtTarget, {
+        enableTime: true,
+        minuteIncrement: 30,
+        time_24hr: true,
+        defaultDate: this.endedAtTarget.value,
+        minDate: this.startedAtTarget.value,
+      });
+
+      flatpickr(this.startedAtTarget, {
+        enableTime: true,
+        minuteIncrement: 30,
+        time_24hr: true,
+        defaultDate: this.startedAtTarget.value,
+        onChange: (selectedDates) => {
+          flatpickr(this.endedAtTarget, {
+            enableTime: true,
+            minuteIncrement: 30,
+            defaultDate: selectedDates[0],
+            minDate: selectedDates[0],
+          });
+        },
+      });
+    }
   }
 }
