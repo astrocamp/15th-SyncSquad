d9464284bf4bc47ce0d44c16197920f6c6f99521
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-23 04:17:01 +0800

    Add user's unread

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @users = User.where(company: current_user.company).all_except(current_user)
 
+    @single_chat = Room.joins(:participants)
+                       .where(room_type: 'single_room',
+                              participants: { user_id: current_user.id })
     @room = Room.new
     @rooms = Room.where(room_type: 'public_room', company: current_user.company)
     @room_name = get_name(@user, current_user)
-    @single_room = Room.find_by(name: @room_name) || Room.create_private_room(current_user, [@user, current_user],
-                                                                              @room_name)
+    @single_room = Room.find_by(name: @room_name) || Room.create_private_room(current_user, [@user, current_user],@room_name)
+    @unread_count = @single_room.unread_messages_count(current_user)
 
     @private_groups = Room.joins(:participants)
                           .where(room_type: 'private_room',
                                  participants: { user_id: current_user.id })
+
+    visit = RoomVisit.find_or_initialize_by(user_id: @current_user.id, room_id: @single_room.id)
+    visit.last_visited_at = Time.current
+    visit.save if visit.changed?
+
     @message = Message.new
 
     @messages = @single_room.messages.includes(:user).order(:created_at)
