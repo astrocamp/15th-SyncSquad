355a1eaf8f0a12c686e880dd8685ac8629ab7274
AranceLiu <fish70309@gmail.com>
2024-01-22 22:27:41 +0800

    Distinguish: Upgrade or not for company with crown beside on name

--- app/javascript/lib/fontawesome.js
+++ app/javascript/lib/fontawesome.js
@@ -1,19 +1,21 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faEllipsis } from '@fortawesome/free-solid-svg-icons/faEllipsis'
-import { faBuilding } from '@fortawesome/free-solid-svg-icons/faBuilding'
-import { faUser } from '@fortawesome/free-solid-svg-icons/faUser'
-import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons/faAngleRight'
-import { faAngleLeft } from '@fortawesome/free-solid-svg-icons/faAngleLeft'
-import { faChartSimple } from '@fortawesome/free-solid-svg-icons/faChartSimple'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons/faLocationDot'
-import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck'
-import { faAnglesUp } from '@fortawesome/free-solid-svg-icons/faAnglesUp'
-import { faPen } from '@fortawesome/free-solid-svg-icons/faPen'
-import { faPenToSquare as faPenToSquareSolid } from '@fortawesome/free-solid-svg-icons/faPenToSquare'
-import { faCalendar } from '@fortawesome/free-regular-svg-icons/faCalendar'
-import { faTrashCan } from '@fortawesome/free-regular-svg-icons/faTrashCan'
-import { faPenToSquare as faPenToSquareRegular } from '@fortawesome/free-regular-svg-icons/faPenToSquare'
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons/faEllipsis";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons/faBuilding";
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons/faAngleRight";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons/faAngleLeft";
+import { faChartSimple } from "@fortawesome/free-solid-svg-icons/faChartSimple";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons/faLocationDot";
+import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+import { faAnglesUp } from "@fortawesome/free-solid-svg-icons/faAnglesUp";
+import { faPen } from "@fortawesome/free-solid-svg-icons/faPen";
+import { faPenToSquare as faPenToSquareSolid } from "@fortawesome/free-solid-svg-icons/faPenToSquare";
+import { faCalendar } from "@fortawesome/free-regular-svg-icons/faCalendar";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons/faTrashCan";
+import { faPenToSquare as faPenToSquareRegular } from "@fortawesome/free-regular-svg-icons/faPenToSquare";
+import { faCircleChevronLeft } from "@fortawesome/free-solid-svg-icons/faCircleChevronLeft";
+import { faCrown } from "@fortawesome/free-solid-svg-icons/faCrown";
 
 library.add({
   faEllipsis,
@@ -31,6 +33,8 @@ library.add({
   faAnglesUp,
   faPenToSquareSolid,
   faPenToSquareRegular,
-})
+  faCircleChevronLeft,
+  faCrown,
+});
 
-dom.watch()
+dom.watch();
