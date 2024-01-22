c34210206076a27169932b9b4c108298f55559ae
AranceLiu <fish70309@gmail.com>
2024-01-22 21:27:45 +0800

    Update: status after paid and authorize for order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
+  before_action :require_login, only: %i[index]
   def index
     @order = Order.new
+    authorize @order
   end
 
   def create
@@ -14,6 +16,7 @@ class OrdersController < ApplicationController
     if response && response.parsed_response['returnCode'] == '0000'
       payment_url = response.parsed_response['info']['paymentUrl']['web']
       Rails.logger.info "Redirecting to LINE Pay at #{payment_url}"
+      current_company.update(paid: true)
       redirect_to payment_url, allow_other_host: true
       flash[:success] = t('orders.create_success')
 
@@ -22,5 +25,13 @@ class OrdersController < ApplicationController
       render :index
       flash[:success] = '付款失敗！請再次確認'
     end
+  end
+
+  private
+
+  def require_login
+    return if current_company || current_user
+
+    redirect_to root_path, alert: t('orders.login')
   end
 end
