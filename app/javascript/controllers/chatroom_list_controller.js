f885485cb99f34c35f5284b2a3640e5ca796d516
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-25 15:03:23 +0800

    WIP remove drawer and renew JS

--- app/javascript/controllers/chatroom_list_controller.js
+++ app/javascript/controllers/chatroom_list_controller.js
@@ -1,9 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["usersList", "roomsList"];
+  static targets = ["usersList", "roomsList", "leftSide"];
+
+  connect() {
+    console.log(window.screen.width < 768);
+  }
 
   showUsers() {
+    console.log(this.leftSideTarget);
+    if (window.screen.width < 768) {
+      this.leftSideTarget.classList.remove("hidden");
+    } else {
+      this.leftSideTarget.classList.add("hidden");
+    }
+
     this.usersListTarget.classList.remove("hidden");
     this.roomsListTarget.classList.add("hidden");
   }
