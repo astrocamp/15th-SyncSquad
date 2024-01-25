94956186d8f8fcff74acbf4ad67c0329134374ff
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2024-01-25 12:49:18 +0800

    fix comment create turbo_stream & project layout user photo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,14 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
-    task = Task.find(params[:task_id])
-    comment = task.comments.new(comment_params)
-    @comments = task.comments
-    return if comment.save
-
-    redirect_to tasks_path(task)
+    @task = Task.find(params[:task_id])
+    comment = @task.comments.new(comment_params)
+    @comments = @task.comments
+    
+    unless comment.save
+      flash.now[:alert] = t('comment.context_blank_alert')
+      render turbo_stream: turbo_stream.replace('comment-form', partial: 'comments/form', locals: { comment: comment })
+    end
   end
 
   def destroy
