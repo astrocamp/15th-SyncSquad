927c2f2978350623b1e09964a7cd971eb6ba5294
Tim Chaio <timmy1993jaw@gmail.com>
2024-01-25 19:41:29 +0800

    Fix turbo_stream bug

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,6 @@
 
 class Room < ApplicationRecord
   # validation: room's name could not recurring and create public room
-  validates :name, presence: true, uniqueness: true
   # after_create_commit { broadcast_if_public }
   has_many :messages
   has_many :participants, dependent: :destroy
