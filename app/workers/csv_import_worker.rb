a5c696bb8ec0ca6c1ee5a97244ff44f9858ffc58
AranceLiu <fish70309@gmail.com>
2024-01-25 18:05:20 +0800

    Revert without sidekiq

--- app/workers/csv_import_worker.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CsvImportWorker
-  include Sidekiq::Worker
-  sidekiq_options retry: 5
-  def perform(file_path, company_id, user_id)
-    CsvImportUsersService.new.call(file_path, company_id, user_id)
-  end
-end
