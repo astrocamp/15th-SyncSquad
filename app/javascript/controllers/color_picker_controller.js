1b0f8cc5710ac31742fc37e74e5ba9236ccbccd1
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2024-01-26 16:26:00 +0800

    fix project calendar RWD & task priority tag position & list edit js

--- app/javascript/controllers/color_picker_controller.js
+++ app/javascript/controllers/color_picker_controller.js
@@ -1,30 +1,32 @@
-import ColorPicker from "stimulus-color-picker";
-import { put } from "@rails/request.js";
+import ColorPicker from 'stimulus-color-picker'
+import { put } from '@rails/request.js'
 
 export default class extends ColorPicker {
-  static targets = ["input"];
+  static targets = ['input']
   connect() {
-    super.connect();
-    const defaultColor = "#4299E1";
-    const btn = this.element.querySelector("button");
-    btn.style.setProperty("--pcr-color", defaultColor);
-    this.inputTarget.value = defaultColor;
+    super.connect()
+    if (this.inputTarget.value == '') {
+      const defaultColor = '#4299E1'
+      const btn = this.element.querySelector('button')
+      btn.style.setProperty('--pcr-color', defaultColor)
+      this.inputTarget.value = defaultColor
+    }
 
     // Pickr instance
     this.picker
-      .on("changestop", (source, instance) => {
-        this.updateColorReview(instance);
-        this.onSave(color);
+      .on('changestop', (source, instance) => {
+        this.updateColorReview(instance)
+        this.onSave(color)
       })
-      .on("swatchselect", (color, instance) => {
-        this.updateColorReview(instance);
-        this.onSave(color);
-      });
+      .on('swatchselect', (color, instance) => {
+        this.updateColorReview(instance)
+        this.onSave(color)
+      })
   }
 
   // Callback when the color is saved
   onSave(color) {
-    super.onSave(color);
+    super.onSave(color)
   }
 
   // You can override the components options with this getter.
@@ -39,17 +41,17 @@ export default class extends ColorPicker {
         clear: false,
         save: false,
       },
-    };
+    }
   }
 
   updateColorReview(instance) {
     instance._eventBindings[6][0].style.setProperty(
-      "--pcr-color",
+      '--pcr-color',
       instance.getColor().toHEXA().toString()
-    );
+    )
   }
 
   get swatches() {
-    return ["#F56565", "#ED8936", "#ECC94B", "#48BB78", "#4299E1", "#9F7AEA"];
+    return ['#F56565', '#ED8936', '#ECC94B', '#48BB78', '#4299E1', '#9F7AEA']
   }
 }
